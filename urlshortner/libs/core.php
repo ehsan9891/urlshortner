e78ba823ff99f3b6c04a4db5b4c7ed406569353a
Ehsan Razmkhah <ehsan9891@gmail.com>
2015-11-28 20:12:36 +0330

    Added Modular Functionality
    Added a sample module which made the full system modular. for easier maintance

--- urlshortner/libs/core.php
+++ urlshortner/libs/core.php
@@ -1,45 +1,76 @@
 <?php
-class core{
+
+class core {
+
     private $base;
     private $DBUSER;
     private $DBNAME;
     private $DBPASS;
     private $DBSERVER;
+
     function __construct() {
-        $this->base = __DIR__ . "/";  
+        $this->base = __DIR__ . "/";
     }
-    function addPreRequest(){
+
+    function addPreRequest() {
         $this->loadConfigFile();
         $this->loadDAL();
-        
     }
-    function Main(){
+
+    function Main() {
         $this->addPreRequest();
         $this->route();
     }
-    function loadTemplate($tpl_name){
-        include_once $this->base.'rain.tpl.class.php';
+
+    function loadTemplate($tpl_name, $modules) {
+
+        include_once $this->base . 'rain.tpl.class.php';
         include_once $this->base . 'tpl.php';
     }
-    function loadDAL(){
+
+    function loadDAL() {
         global $DBConnection;
         include_once $this->base . 'dal.php';
-        $DBConnection =  new MysqliDb ($this->DBSERVER, $this->DBUSER, $this->DBPASS, $this->DBNAME);
-        
+        $DBConnection = new MysqliDb($this->DBSERVER, $this->DBUSER, $this->DBPASS, $this->DBNAME);
     }
-    function route(){
+
+    function route() {
         $url = @$_GET['url'];
-        if(is_null($url)){
-            $this->loadTemplate("uploadTPL");
-        }else{
-            echo "this should be redirected";
+        if (is_null($url)) {
+            $modules = $this->loadModule(1);
+            $this->loadTemplate("uploadTPL", $modules);
+        } else {
+            $this->loadModule(2);
+            $this->loadTemplate("redirectTPL");
         }
     }
-    function loadConfigFile(){
-        include_once $this->base."../config.php";
+
+    function loadModule($url_type) {
+        $modulesBase = $this->base . "../modules/";
+        switch ($url_type) {
+            case 1:
+                $modules['uploader'] = $this->runModule($modulesBase . 'uploader/index.php');
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        return $modules;
+    }
+
+    function loadConfigFile() {
+        include_once $this->base . "../config.php";
         $this->DBNAME = $DBNAME;
         $this->DBPASS = $DBPASS;
         $this->DBSERVER = $DBSERVER;
         $this->DBUSER = $DBUSER;
     }
+
+    function runModule($file) {
+        ob_start();
+        require($file);
+        return ob_get_clean();
+    }
+
 }
--- urlshortner/libs/tpl.php
+++ urlshortner/libs/tpl.php
@@ -3,10 +3,10 @@
 	raintpl::configure("base_url", null );
 	raintpl::configure("tpl_dir", "tpl/".$tpl_name."/" );
 	raintpl::configure("cache_dir", "tmp/" );
-
+        
 	//initialize a Rain TPL object
 	$tpl = new RainTPL;
-
+        $tpl->assign( "uploader", $modules['uploader'] );
 	// you can draw the output
         // $tpl->draw( 'page' );
         // or the template output string by setting $return_string = true:
